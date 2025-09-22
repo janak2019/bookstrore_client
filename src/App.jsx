3d2b1b58bd19ea772720da24f87b637abc504a68
htmlbasic <withjanak@gmail.com>
2025-09-22 13:28:31 +0545

    some changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 // src/App.jsx
-import React from "react";
+import React, { useEffect } from "react";
 import { Router, RouterProvider } from "react-router-dom";
 import router from "./router";
-
+import { getUser } from "./store/slices/authSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchAllUsers } from "./store/slices/userSlice"
+import { fetchAllBooks } from "./store/slices/bookSlice"
 
 
 function App() {
+  const { user,loading, isAuthenticated } = useSelector((state) => state.auth);
+const dispatch = useDispatch();
+
+useEffect(() => { 
+    
+    if(isAuthenticated && user?.role ==="Admin"){
+      dispatch(getUser());
+     dispatch(fetchAllBooks());
+    
+      dispatch(fetchAllUsers());
+      
+    }
+   }, [isAuthenticated]);
+   if(loading) return<div>Loading...</div>
   
 
   return <RouterProvider router={router} />;
