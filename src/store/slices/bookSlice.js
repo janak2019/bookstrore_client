36432ff5fa8985b84dd42a8e9fcd02686e631b9f
htmlbasic <withjanak@gmail.com>
2025-09-22 13:36:47 +0545

    book url fixed

--- src/store/slices/bookSlice.js
+++ src/store/slices/bookSlice.js
@@ -47,7 +47,7 @@ const bookSlice = createSlice({
 
 export const fetchAllBooks = ()=>async(dispatch)=>{
     dispatch(bookSlice.actions.fetchBooksRequest())
-    await axios.get("https://book-store-server-3n9u.onrender.com/api/v1/books/all",{withCredentials:true})
+    await axios.get("https://book-store-server-3n9u.onrender.com/api/v1/books",{withCredentials:true})
     .then(res=>{
         dispatch(bookSlice.actions.fetchBooksSuccess(res.data.books))
     })
@@ -57,7 +57,7 @@ export const fetchAllBooks = ()=>async(dispatch)=>{
 }
 export const addBook = (data) =>async(dispatch)=>{
     dispatch(bookSlice.actions.addBookRequest())
-    await axios.post("https://book-store-server-3n9u.onrender.com/api/v1/books/admin/add",data,{
+    await axios.post("https://book-store-server-3n9u.onrender.com/api/v1/books",data,{
         withCredentials: true,
         headers:{
             "Content-Type":"application/json"
